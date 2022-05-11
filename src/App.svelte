023a208a40787e48e9681ac25417f1a63aa320e0
XtratoS <mamdouh.waleed.96@gmail.com>
2022-05-12 00:26:49 +0200

    move World to Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import World from "./assets/World.svelte";
+	import World from "./Components/World.svelte";
 </script>
 
 <World />
 
 <style>
 	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 	}
 </style>
