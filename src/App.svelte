19ac86766087ffec03b4c7f2d65fdcff045d4c2b
XtratoS <mamdouh.waleed.96@gmail.com>
2022-05-12 00:20:41 +0200

    Initial#

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import World from "./assets/World.svelte";
+</script>
+
+<World />
+
+<style>
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+</style>
